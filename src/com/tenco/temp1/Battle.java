bfdf0dccaadf9b091879fc261e4cb5a8bc141cd0
ning00060 <ning00060@gmail.com>
2024-04-18 17:10:06 +0900

    place

--- src/com/tenco/temp1/Battle.java
+++ src/com/tenco/temp1/Battle.java
@@ -3,6 +3,10 @@ package com.tenco.temp1;
 public class Battle {
 
 	public static void main(String[] args) {
+		Place place1_1 = new Place("동쪽","공원");
+		Place place1_2 = new Place("동쪽","병원");
+		Place place2 = new Place("서쪽","공원");
+		Place place3 = new Place("남쪽","공원");
 
 		Fire fire = new Fire("파이리");
 		Water water = new Water("꼬부기");
---
+++ src/com/tenco/temp1/Place.java
@@ -0,0 +1,49 @@
+package com.tenco.temp1;
+
+public class Place {
+	private String name;
+	private String spot;
+	private String gym;
+	
+	
+	public Place(String name,String spot) {
+		this.name=name;
+		this.spot=spot;
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpot() {
+		return spot;
+	}
+
+	public void setSpot(String spot) {
+		this.spot = spot;
+	}
+
+	public String getGym() {
+		return gym;
+	}
+
+	public void setGym(String gym) {
+		this.gym = gym;
+	}
+	
+}
