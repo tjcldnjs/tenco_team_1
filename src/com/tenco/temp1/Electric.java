61c186b825105bbd064c5f3056a8879bf1617a28
kdt4 <masun5737@naver.com>
2024-04-18 16:51:36 +0900

    1차

---
+++ src/com/tenco/temp1/Electric.java
@@ -0,0 +1,54 @@
+package com.tenco.temp1;
+
+public class Electric {
+
+	String name;
+	int power;
+	int hp;
+	int mp;
+	
+	public Electric(String name) {
+		this.name = name ;
+		power = 6;
+		hp = 50;
+		mp = 40;
+	}
+	// Fire 공격하기
+	
+	public void attakFire(Fire f) {
+		System.out.println(this.name + " 이 " + f.name + " 를 데미지 " + this.power + " 만큼 공격합니다.");
+		f.beAttackted(this.power);
+		if(mp <= 0) {
+			System.out.println(" MP 가 없습니다.");
+			return;
+		}
+		mp -= 5;
+	}
+	
+	// Water 공격하기
+	
+	public void attakWater(Water w) {
+		System.out.println(this.name + " 이 " + w.name + " 를 데미지 " + this.power + " 만큼 공격합니다.");
+		w.beAttackted(this.power);
+		if(mp <= 0) {
+			System.out.println(" MP 가 없습니다.");
+			return;
+		}
+		mp -= 5;
+	}
+
+	// 내가 공격받기
+	public void beAttackted(int power) {
+		if (hp <= 0) {
+			System.out.println(" 죽었습니다. ");
+			return;
+		}
+		hp = hp -power;
+	}	
+	public void showInfo() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("공격력 : " + this.power);
+		System.out.println("남은 hp : " + this.hp);
+		System.out.println("남은 mp : " + this.mp);
+	}
+}
---
+++ src/com/tenco/temp1/Water.java
@@ -0,0 +1,55 @@
+package com.tenco.temp1;
+
+public class Water {
+
+	String name;
+	int power;
+	int hp;
+	int mp;
+	
+	public Water(String name) {
+		this.name = name ;
+		power = 4;
+		hp = 60;
+		mp = 40;
+	}
+		// Fire 공격하기
+		
+		public void attakFire(Fire f) {
+			System.out.println(this.name + " 이 " + f.name + " 를 데미지 " + this.power + " 만큼 공격합니다.");
+			f.beAttackted(this.power);
+			if(mp <= 0) {
+				System.out.println(" MP 가 없습니다.");
+				return;
+			}
+			mp -= 5;
+		}
+		
+		// Electric 공격하기
+		
+		public void attakElectric(Electric e) {
+			System.out.println(this.name + " 이 " + e.name + " 를 데미지 " + this.power + " 만큼 공격합니다.");
+			e.beAttackted(this.power);
+			if(mp <= 0) {
+				System.out.println(" MP 가 없습니다.");
+				return;
+			}
+			mp -= 5;
+		}
+
+		// 내가 공격받기
+		public void beAttackted(int power) {
+			if (hp <= 0) {
+				System.out.println(" 죽었습니다. ");
+				return;
+			}
+			hp = hp -power;
+		}
+		
+		public void showInfo() {
+			System.out.println("이름 : " + this.name);
+			System.out.println("공격력 : " + this.power);
+			System.out.println("남은 hp : " + this.hp);
+			System.out.println("남은 mp : " + this.mp);
+		}
+}
